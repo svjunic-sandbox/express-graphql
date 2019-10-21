b83a6c803bbb8f2a14edf318dacb8e4e2a2dc05a
sv.junic <sv.junic@gmail.com>
2019-10-21 20:23:57 +0900

    work1

--- README.md
+++ README.md
@@ -1,3 +1,57 @@
 # sandbox-express-graphql
 GraphQLのサイトに書いてあるexpress-graphqlを使ったソースからの確認用
 共有用。
+
+
+## 確認用クエリ
+```
+{
+  server_time
+  qiita (page:1,per_page:1,query:"svjunic"),{
+    #rendered_body
+    #body
+    coediting
+    comments_count
+    created_at
+    id
+    likes_count
+    private
+    reactions_count
+    title
+    updated_at
+    url
+    user {
+      description
+      facebook_id
+      followees_count
+      followers_count
+      github_login_name
+      id
+      items_count
+      linkedin_id
+      location
+      name
+      organization
+      permanent_id
+      profile_image_url
+      team_only
+      twitter_screen_name
+      website_url
+    }
+    page_views_count
+    tags {
+      name
+      versions
+    }
+    group {
+      created_at
+      id
+      name
+      private
+      updated_at
+      url_name
+    }
+  }
+}
+```
+
--- server.js
+++ server.js
@@ -2,22 +2,87 @@ let express = require('express');
 let graphqlHTTP = require('express-graphql');
 let { buildSchema } = require('graphql');
 
+let axios = require('axios');
+
+// https://qiita.com/api/v2/docs#get-apiv2users
+
 // Construct a schema, using GraphQL schema language
 let schema = buildSchema(`
   type Query {
-    server_time: String,
-    hello: String
+    server_time: String!
+    qiita(page: Int, per_page: Int,query:String): [Qiita]
+  }
+
+  type Qiita {
+    rendered_body    : String
+    body             : String
+    coediting        : Boolean
+    comments_count   : Int
+    created_at       : String
+    id               : String
+    likes_count      : Int
+    private          : Boolean
+    reactions_count  : Int
+    title            : String
+    updated_at       : String
+    url              : String
+    user             : UserObject
+    page_views_count : Int
+    tags             : [Tag]
+    group            : Group
+  }
+
+  type Group {
+    created_at : String
+    id         : Int
+    name       : String
+    private    : Boolean
+    updated_at : String
+    url_name   : String
+  }
+
+  type Tag {
+    name     : String
+    versions : [ String ]
+  }
+
+  type UserObject {
+    description         : String
+    facebook_id         : String
+    followees_count     : Int
+    followers_count     : Int
+    github_login_name   : String
+    id                  : String
+    items_count         : Int
+    linkedin_id         : String
+    location            : String
+    name                : String
+    organization        : String
+    permanent_id        : Int
+    profile_image_url   : String
+    team_only           : Boolean
+    twitter_screen_name : String
+    website_url         : String
   }
 `);
 
 // The root provides a resolver function for each API endpoint
 let root = {
-  hello: () => {
-    return 'Hello world!';
-  },
   server_time: () => {
     const mogeta = new Date().toGMTString();
     return mogeta;
+  },
+  server_time: () => {
+    const server_time = new Date().toGMTString();
+    return server_time;
+  },
+  qiita: async ({ page = 1, per_page = 30, query = 'svjunic' }) => {
+    // qiitaのAPI使わせてもらう
+    // https://qiita.com/api/v2/docs#get-apiv2items
+    // 'https://qiita.com/api/v2/items',
+    let response = await axios.get(`https://qiita.com/api/v2/items?page=${page}&per_page=${per_page}&query=${query}`);
+
+    return response.data;
   }
 };
 
